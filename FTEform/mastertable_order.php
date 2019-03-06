e8ff2d789c2f8aaf56f518c4806bd49ac2c345a9
Víctor <victamen92@gmail.com>
2019-03-06 11:45:58 +0100

    Add files via upload

--- FTEform/mastertable_order.php
+++ FTEform/mastertable_order.php
@@ -30,7 +30,8 @@ if(mysqli_connect_error()) {
 } 
 else {  
 	//Multiply hours *2.4 to get from 100 minutes format to 60 minutes format
-	$SELECT = "SELECT orden, airplane, insp, id, cert, MIN(fecha), estado, cierre FROM ftedata 
+	$SELECT = "SELECT orden, airplane, insp, id, hs1, he1, hs2, he2, hs3, he3, hs4, he4, hs5, he5, cert, 
+    MIN(fecha), estado, cierre FROM ftedata 
     WHERE orden LIKE '$mat' 
     AND airplane LIKE '$airplane' 
     AND fecha BETWEEN '$fechas' AND '$fechae'
@@ -43,7 +44,8 @@ else {
     //Prepare statement
    	$stmt = $mysqli->prepare($SELECT);
     $stmt->execute();
-    $stmt->bind_result($orden, $airplane, $insp, $id, $cert, $fecha, $estado, $cierre);
+    $stmt->bind_result($orden, $airplane, $insp, $id, $hs1, $he1, $hs2, $he2, $hs3, $he3, $hs4, $he4, $hs5, $he5,
+        $cert, $fecha, $estado, $cierre);
     $stmt->store_result();
     $rnum = $stmt->num_rows;
 ?>
@@ -58,6 +60,16 @@ else {
                     <th>Avión</th> 
                     <th>Inspección</th> 
                     <th>Mecánico</th>
+                    <th>Turno 1 ini</th>
+                    <th>Turno 1 fini</th>
+                    <th>Turno 2 ini</th>
+                    <th>Turno 2 fini</th>
+                    <th>Turno 3 ini</th>
+                    <th>Turno 3 fini</th>
+                    <th>Turno 4 ini</th>
+                    <th>Turno 4 fini</th>
+                    <th>Turno 5 ini</th>
+                    <th>Turno 5 fini</th>
                     <th>Certificador</th> 
                     <th>Fecha inicio</th> 
                     <th>Estado</th> 
@@ -76,6 +88,16 @@ else {
                     <td><?php printf("%s", $airplane);?></td>
                     <td><?php printf("%s", $insp);?></td>
                     <td><?php printf("%s", $id);?></td>
+                    <td><?php printf("%s", $hs1);?></td>
+                    <td><?php printf("%s", $he1);?></td>
+                    <td><?php printf("%s", $hs2);?></td>
+                    <td><?php printf("%s", $he2);?></td>
+                    <td><?php printf("%s", $hs3);?></td>
+                    <td><?php printf("%s", $he3);?></td>
+                    <td><?php printf("%s", $hs4);?></td>
+                    <td><?php printf("%s", $he4);?></td>
+                    <td><?php printf("%s", $hs5);?></td>
+                    <td><?php printf("%s", $he5);?></td>
                     <td><?php printf("%s", $cert);?></td>
                     <td><?php printf("%s", $fecha);?></td>
                     <td><?php printf("%s", $estado);?></td>
--- FTEform/mechupdate.php
+++ FTEform/mechupdate.php
@@ -15,22 +15,163 @@ $_SESSION["mechname"];
 $_SESSION["orden"];
 $_SESSION["fecha"];
 
-$id = $_SESSION["mechname"];
-$orden = $_SESSION["orden"];
-$fecha = $_SESSION["fecha"];
+$orden_comp = $_SESSION["orden"];
+$fecha_comp = $_SESSION["fecha"];
+$id_comp = $_SESSION["mechname"];
+
+$orden = $mysqli->real_escape_string($_POST["orden"]);
+$fecha = $mysqli->real_escape_string($_POST["fecha"]);
+$id = $mysqli->real_escape_string($_POST["mechname"]);
+
+$hs1 = $mysqli->real_escape_string($_POST['hs1h'].''.$_POST['hs1m'].'00');
+$he1 = $mysqli->real_escape_string($_POST['he1h'].''.$_POST['he1m'].'00');
+$hs2 = $mysqli->real_escape_string($_POST['hs2h'].''.$_POST['hs2m'].'00');
+$he2 = $mysqli->real_escape_string($_POST['he2h'].''.$_POST['he2m'].'00');
+$hs3 = $mysqli->real_escape_string($_POST['hs3h'].''.$_POST['hs3m'].'00');
+$he3 = $mysqli->real_escape_string($_POST['he3h'].''.$_POST['he3m'].'00');
+$hs4 = $mysqli->real_escape_string($_POST['hs4h'].''.$_POST['hs4m'].'00');
+$he4 = $mysqli->real_escape_string($_POST['he4h'].''.$_POST['he4m'].'00');
+$hs5 = $mysqli->real_escape_string($_POST['hs5h'].''.$_POST['hs5m'].'00');
+$he5 = $mysqli->real_escape_string($_POST['he5h'].''.$_POST['he5m'].'00');
+
+$datos = $mysqli->real_escape_string($_POST['datos']);
+
+$datos1 = $mysqli->real_escape_string($_POST['datos1']);
+$datos2 = $mysqli->real_escape_string($_POST['datos2']);
+$datos3 = $mysqli->real_escape_string($_POST['datos3']);
+$datos4 = $mysqli->real_escape_string($_POST['datos4']);
+$datos5 = $mysqli->real_escape_string($_POST['datos5']);
+
+$airplane = $mysqli->real_escape_string($_POST['airplane']);
+$insp = $mysqli->real_escape_string($_POST['insp']);
 
 //Si hay error en la conexión.
 if(mysqli_connect_error()) {
 	die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
 } 
 else {   
-  	$INSERT = "INSERT INTO ftedata (id, orden, fecha, cert, airplane, insp, datos,
-    hs1, he1, datos1, hs2, he2, datos2, hs3, he3, datos3, hs4, he4, datos4, hs5, he5, datos5, estado) 
-    	VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $INSERT_ORDER = "INSERT INTO ftecp (ordenc) VALUES(?)";
+    $INSERT_DATE = "INSERT INTO ftecp (fechac) VALUES(?)";
+    $INSERT_ID = "INSERT INTO ftecp (idc) VALUES(?)";
 
-    $INSERTCP = "INSERT INTO ftecp (idc, ordenc, fechac, certc, airplanec, inspc, datosc,
-    hs1c, he1c, datos1c, hs2c, he2c, datos2c, hs3c, he3c, datos3c, hs4c, he4c, datos4c, hs5c, he5c, datos5c, estadoc) 
-    	VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $INSERT_HS1 = "INSERT INTO ftecp (hs1c) VALUES(?)";
+    $INSERT_HE1 = "INSERT INTO ftecp (he1c) VALUES(?)";
+    $INSERT_HS2 = "INSERT INTO ftecp (hs2c) VALUES(?)";
+    $INSERT_HE2 = "INSERT INTO ftecp (he2c) VALUES(?)";
+    $INSERT_HS3 = "INSERT INTO ftecp (hs3c) VALUES(?)";
+    $INSERT_HE3 = "INSERT INTO ftecp (he3c) VALUES(?)";
+    $INSERT_HS4 = "INSERT INTO ftecp (hs4c) VALUES(?)";
+    $INSERT_HE4 = "INSERT INTO ftecp (he4c) VALUES(?)";
+    $INSERT_HS5 = "INSERT INTO ftecp (hs5c) VALUES(?)";
+    $INSERT_HE5 = "INSERT INTO ftecp (he5c) VALUES(?)";
+
+    $INSERT_PLANE = "INSERT INTO ftecp (airplanec) VALUES(?)";
+    $INSERT_INSP = "INSERT INTO ftecp (inspc) VALUES(?)";
+    $INSERT_DATOS = "INSERT INTO ftecp (datosc) VALUES(?)";
+
+    $INSERT_DATOS1 = "INSERT INTO ftecp (datos1c) VALUES(?)";
+    $INSERT_DATOS2 = "INSERT INTO ftecp (datos2c) VALUES(?)";
+    $INSERT_DATOS3 = "INSERT INTO ftecp (datos3c) VALUES(?)";
+    $INSERT_DATOS4 = "INSERT INTO ftecp (datos4c) VALUES(?)";
+    $INSERT_DATOS5 = "INSERT INTO ftecp (datos5c) VALUES(?)";
+
+
+
+    $UPDATE_ORDER = "UPDATE ftedata, ftecp SET 
+    ftedata.orden = ftecp.ordenc WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.ordenc != ''";
+
+    $UPDATE_FECHA = "UPDATE ftedata, ftecp SET 
+    ftedata.fecha = ftecp.fechac WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.fechac != ''";
+
+    $UPDATE_ID = "UPDATE ftedata, ftecp SET 
+    ftedata.id = ftecp.idc WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.idc != ''";
+
+    
+
+    $UPDATE_HS1 = "UPDATE ftedata, ftecp SET 
+    ftedata.hs1 = ftecp.hs1c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.hs1c != ''";
+
+    $UPDATE_HE1 = "UPDATE ftedata, ftecp SET 
+    ftedata.he1 = ftecp.he1c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.he1c != ''";
+
+    $UPDATE_HS2 = "UPDATE ftedata, ftecp SET 
+    ftedata.hs2 = ftecp.hs2c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.hs2c != ''";
+
+    $UPDATE_HE2 = "UPDATE ftedata, ftecp SET 
+    ftedata.he2 = ftecp.he2c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.he2c != ''";
+
+    $UPDATE_HS3 = "UPDATE ftedata, ftecp SET 
+    ftedata.hs3 = ftecp.hs3c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.hs3c != ''";
+
+    $UPDATE_HE3 = "UPDATE ftedata, ftecp SET 
+    ftedata.he3 = ftecp.he3c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.he3c != ''";
+
+    $UPDATE_HS4 = "UPDATE ftedata, ftecp SET 
+    ftedata.hs4 = ftecp.hs4c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.hs4c != ''";
+
+    $UPDATE_HE4 = "UPDATE ftedata, ftecp SET 
+    ftedata.he4 = ftecp.he4c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.he4c != ''";
+
+    $UPDATE_HS5 = "UPDATE ftedata, ftecp SET 
+    ftedata.hs5 = ftecp.hs5c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.hs5c != ''";
+
+    $UPDATE_HE5 = "UPDATE ftedata, ftecp SET 
+    ftedata.he5 = ftecp.he5c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.he5c != ''";
+
+
+
+    $UPDATE_PLANE = "UPDATE ftedata, ftecp SET 
+    ftedata.airplane = ftecp.airplanec WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.airplanec != ''";
+
+    $UPDATE_INSP = "UPDATE ftedata, ftecp SET 
+    ftedata.insp = ftecp.inspc WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.inspc != ''";
+
+    $UPDATE_DATOS = "UPDATE ftedata, ftecp SET 
+    ftedata.datos = ftecp.datosc WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.datosc != ''";
+
+
+
+    $UPDATE_DATOS1 = "UPDATE ftedata, ftecp SET 
+    ftedata.datos1 = ftecp.datos1c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.datos1c != ''";
+
+    $UPDATE_DATOS2 = "UPDATE ftedata, ftecp SET 
+    ftedata.datos2 = ftecp.datos2c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.datos2c != ''";
+
+    $UPDATE_DATOS3 = "UPDATE ftedata, ftecp SET 
+    ftedata.datos3 = ftecp.datos3c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.datos3c != ''";
+
+    $UPDATE_DATOS4 = "UPDATE ftedata, ftecp SET 
+    ftedata.datos4 = ftecp.datos4c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.datos4c != ''";
+
+    $UPDATE_DATOS5 = "UPDATE ftedata, ftecp SET 
+    ftedata.datos5 = ftecp.datos5c WHERE ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' 
+    AND ftedata.id = '$id_comp' AND ftecp.datos5c != ''";
+
+
+
+    $DELETE_RECORD = "DELETE FROM ftedata WHERE 
+    ftedata.orden = '$orden_comp' AND ftedata.fecha = '$fecha_comp' AND ftedata.id = '$id_comp'";
+
 
     			
    	//Inserto en tabla comparativa.
